f1338b521e322c0a518ce0c0befbf132ee56ba08
Braden Currah <bacurrah@csuchico.edu>
2025-09-08 11:30:54 -0700

    fix div by zero (closes #61)

--- main.cpp
+++ main.cpp
@@ -15,8 +15,13 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    if (y == 0) {
+      cout << "Division: " << "Dividing by zero is not a number." << endl;
+      cout << "Remainder: " << "Dividing by zero is not a number." << endl;
+    } else {
+      cout << "Division: " << x / y << endl;
+      cout << "Remainder: " << x % y << endl;
+    }
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
