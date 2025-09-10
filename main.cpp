01b61def41737f307b12ced021fcbab70ca3d4e9
Braden Currah <bacurrah@csuchico.edu>
2025-09-10 11:25:52 -0700

    add namespace identifiers

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ int main()
 
     int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
+    std::cin >> x >> y;
+    std::cout << "Addition: " << x + y << std::endl;
+    std::cout << "Subtraction: " << x - y << std::endl;
+    std::cout << "Multiplication: " << x * y << std::endl;
     if (y == 0) {
-      cout << "Division: " << "Dividing by zero is not a number." << endl;
-      cout << "Remainder: " << "Dividing by zero is not a number." << endl;
+      std::cout << "Division: " << "Dividing by zero is not a number." << std::endl;
+      std::cout << "Remainder: " << "Dividing by zero is not a number." << std::endl;
     } else {
-      cout << "Division: " << x / y << endl;
-      cout << "Remainder: " << x % y << endl;
+      std::cout << "Division: " << x / y << std::endl;
+      std::cout << "Remainder: " << x % y << std::endl;
     }
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    std::cout << "Square Root: " << sqrt(x) << std::endl;
+    std::cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
